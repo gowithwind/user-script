170bc89a60d708ecb9e720b7899ca72a553d17c2
gowithwind <jiangfrom2010@gmail.com>
2021-03-30 21:50:01 +0800

    Create scrolltobottom.user.js

---
+++ scrolltobottom.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Scroll to bottom
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://github.com/*/notes/issues/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.scrollTo(0,document.body.scrollHeight);
+    console.log("Scroll to bottom");
+    // Your code here...
+})();
