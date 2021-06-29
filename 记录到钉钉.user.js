18fcf3a46074426e72f466020014d1782f0b6d94
gowithwind <jiangfrom2010@gmail.com>
2021-06-29 22:43:45 +0800

    Update 记录到钉钉.user.js

--- 记录到钉钉.user.js
+++ 记录到钉钉.user.js
@@ -8,8 +8,9 @@
 // @grant         GM.xmlHttpRequest
 // @connect      oapi.dingtalk.com
 // @grant      GM_registerMenuCommand
+// @grant       GM_notification
 // ==/UserScript==
-GM_registerMenuCommand('Run this now', function() {
+GM_registerMenuCommand('记录', function() {
     record();
 }, 'r');
 
@@ -17,19 +18,20 @@ function record(){
     var data = {
         "msgtype": "text",
         "text": {
-            "content": "主人:\n"+location.href
+            "content": "主人:\n"+document.title+'\n'+location.href
         }
     }
     console.log(data);
     GM.xmlHttpRequest({
         method: "POST",
-        url: "https://oapi.dingtalk.com/robot/send?access_token=",
+        url: "https://oapi.dingtalk.com/robot/send?access_token=你的token",
         data: JSON.stringify(data),
         headers: {
             "Content-Type": "application/json"
         },
         onload: function(response) {
             console.log(response.responseText);
+            GM_notification ( {title: '已记录', text: document.title+'\n'+response.responseText} );
         }
     });
 }
