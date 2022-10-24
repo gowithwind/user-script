7942cf127fc4f910d5a00136004c66b00af217f4
gowithwind <jiangfrom2010@gmail.com>
2022-10-24 10:50:40 +0800

    Update 知乎添加发布时间.user.js

--- 知乎添加发布时间.user.js
+++ 知乎添加发布时间.user.js
@@ -11,6 +11,10 @@
 
 (function() {
     'use strict';
-    document.querySelectorAll(".QuestionHeader-title")[1].textContent+=" "+document.querySelectorAll("meta[itemprop=dateCreated]")[0].getAttribute("content").slice(0,10)
+    let create_time = document.querySelectorAll("meta[itemprop=dateCreated]")[0].getAttribute("content").slice(0,10)
+    let span=document.createElement("span")
+    span.style.color="red"
+    span.textContent=create_time
+    document.querySelectorAll(".QuestionHeader-title")[1].appendChild(span)
     // Your code here...
 })();
